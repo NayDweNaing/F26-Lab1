217eb10c8ea55f2f8c2afc2c88781ddfccf5cfd2
NayDweNaing <ndnaing2004@gmail.com>
2026-09-18 16:43:33 +0000

    Lab1b.py Finished

--- lab1b.py
+++ lab1b.py
@@ -6,10 +6,24 @@
 # Purpose: Use arithmetic in python.
 # Usage: python3 lab1b.py
 
-# TO-DO 1:
-#	Create a variable called "num1", take its value from user.
-#	Create another variable called "num2" and take its value from user. 
-# Convert the values to integers using int() function
-num1 = 
-# TO-DO 2:
-# Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
+
+num1 = input("Enter the first number: ");
+num2 = input("Enter the second number: ");
+num1 = int(num1);
+num2 = int(num2);
+addition = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+power = num1 ** num2;
+quotient = num1 / num2;
+floor_division = num1 // num2;
+remainder = num1 % num2;
+
+print("num1 + num2 =", num1+num2);
+print("num1 - num2 =", num1-num2);
+print("num1 * num2 =", num1*num2);
+print("num1 ** num2 =", num1**num2);
+print("num1 / num2 =", num1/num2);
+print("num1 // num2 =", num1//num2);
+print("num1 % num2 =", num1%num2);
+
